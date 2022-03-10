5babd348f34ad48db6bb38ed2da85d4d676ea584
ArielAaronR <aaronresurreccion@gmail.com>
2022-03-10 15:33:35 -0800

    Day 1 of JS30

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', (e) => {
+  let keyCode = e.keyCode;
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; //
+  audio.play();
+  key.classList.add('playing');
+});
+
+const keys = document.querySelectorAll('.key');
+const removeTransition = (e, key) => {
+  if (e.propertyName !== 'transform') return;
+  key.classList.remove('playing');
+};
+
+keys.forEach(
+  (key) =>
+    key.addEventListener('transitionend', (e) => removeTransition(e, key)) //transition end event
+);
